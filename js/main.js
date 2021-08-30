1ddfad59b0259d484d35baf6e5e89f7a745819b2
Ikhsan Nur <ikhsan15nur@gmail.com>
2021-08-30 16:51:04 +0700

    create education section & main.js
    thank u so much to "The WebShala" was solving my problem so fast
    
    https://www.youtube.com/channel/UC94lpQZvaokkIqU-TvtsGXQ
    https://www.facebook.com/The-WebShala-1094349444089751/?ref=page_internal

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// about section tabs
+
+(() => {
+  const aboutSection = document.querySelector(".about-section"),
+  tabsContainer = document.querySelector(".about-tabs");
+
+  tabsContainer.addEventListener("click", (event) =>{
+    // if event.target contains 'tab-item' class and not contains 'active' class
+    if(event.target.classList.contains("tab-item") && 
+      !event.target.classList.contains("active")){
+      const target = event.target.getAttribute("data-target");
+      // console.log(target);
+      // deactive existing active 'tab-item'
+      tabsContainer.querySelector(".active").classList.remove("outer-shadow", "active");
+      // active new 'tab-item'
+      event.target.classList.add("active", "outer-shadow");
+      // deactive existing active 'tab-content'
+      aboutSection.querySelector(".tab-content.active").classList.remove("active");
+      // active new 'tab-content'
+      aboutSection.querySelector(target).classList.add("active");
+    }
+  })
+})();
