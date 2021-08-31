64056f83871570e809aee97289227edaae934c30
Ikhsan Nur <ikhsan15nur@gmail.com>
2021-08-31 17:03:42 +0700

    portfolio section (part 1)

--- js/main.js
+++ js/main.js
@@ -21,3 +21,41 @@
     }
   })
 })();
+
+// portofolio filter and popup
+
+(() =>{
+  const filterContainer = document.querySelector(".portfolio-filter"),
+  portflioItemsContainer = document.querySelector(".portfolio-items"),
+  portfolioItems = document.querySelectorAll(".portfolio-item"),
+  popup = document.querySelector(".portfolio-popup"),
+  prevBtn = popup.querySelector(".pp-prev"),
+  nextBtn = popup.querySelector(".pp-next"),
+  closeBtn = popup.querySelector(".pp-close"),
+  projectDetailsContainer = popup.querySelector(".pp-details"),
+  projectDetailsBtn = popup.querySelector(".pp-project-details-btn");
+  let itemIndex, slideIndex, screenshots;
+
+  // filter portfolio items
+  filterContainer.addEventListener("click", ()=>{
+    if(event.target.classList.contains("filter-item") && 
+    !event.target.classList.contains("active")){
+      // deactive existing active 'filter-item'
+      filterContainer.querySelector(".active").classList.remove("outer-shadow", "active");
+      // active new 'filter item'
+      event.target.classList.add("active", "outer-shadow");
+      const target = event.target.getAttribute("data-target");
+      portfolioItems.forEach((item)=>{
+        if(target === item.getAttribute("data-category") || target === 'all'){
+          item.classList.remove("hide");
+          item.classList.add("show");  
+        }
+        else{
+          item.classList.remove("show");
+          item.classList.add("hide");
+        }
+      })
+    }
+  })
+
+})();
