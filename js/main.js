2b646dd184d7486277ee079e084b54966053de96
Ikhsan Nur <ikhsan15nur@gmail.com>
2021-09-02 16:30:55 +0700

    create testimonial section

--- js/main.js
+++ js/main.js
@@ -175,3 +175,55 @@ function bodyScrollingToggle(){
   }
 
 })();
+
+// testimonial slider
+// () => {} function expression
+(() =>{
+  const sliderContainer = document.querySelector(".testi-slider-container"),
+  slides = sliderContainer.querySelectorAll(".testi-item"),
+  slideWidth = sliderContainer.offsetWidth;
+  prevBtn = document.querySelector(".testi-slider-nav .prev"),
+  nextBtn = document.querySelector(".testi-slider-nav .next"),
+  activeSlide = sliderContainer.querySelector(".testi-item.active");
+  // let slideIndex = 0;
+  let slideIndex = Array.from(activeSlide.parentElement.children).indexOf(activeSlide);
+  // console.log(slideIndex);
+
+  // set width of all slides
+  slides.forEach((slide) =>{
+    slide.style.width = slideWidth + "px";
+  })
+
+  // set width of sliderContainer
+  sliderContainer.style.width = slideWidth * slides.length + "px";
+
+  nextBtn.addEventListener("click", () =>{
+    if(slideIndex === slides.length - 1){
+      slideIndex = 0;
+    }
+    else{
+      slideIndex++;
+    }
+    slider();
+  })
+
+  prevBtn.addEventListener("click", () =>{
+    if(slideIndex === 0){
+      slideIndex = slides.length - 1;
+    }
+    else{
+      slideIndex--;
+    }
+    slider();
+  })
+
+  function slider(){
+    // deactive existing active slide
+    sliderContainer.querySelector(".testi-item.active").classList.remove("active");
+    // active new slide
+    slides[slideIndex].classList.add("active");
+    sliderContainer.style.marginLeft = - (slideWidth * slideIndex) + "px";
+  }
+  slider();
+
+})();
