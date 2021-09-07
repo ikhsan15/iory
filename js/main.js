105b26b15f41015d9651aa5935a22121bcc634a9
Ikhsan Nur <ikhsan15nur@gmail.com>
2021-09-07 17:01:11 +0700

    create responsive, page loader

--- js/main.js
+++ js/main.js
@@ -307,3 +307,11 @@ function bodyScrollingToggle(){
   })
 
 })();
+
+window.addEventListener("load", () =>{
+  // preloader
+  document.querySelector(".preloader").classList.add("fade-out");
+  setTimeout(() =>{
+    document.querySelector(".preloader").style.display="none";
+  }, 600)
+})
