781b8c80943dc9e5ff94e30a4214ed8c1f7deb6a
Ikhsan Nur <ikhsan15nur@gmail.com>
2021-09-08 09:56:49 +0700

    update condition when theme light or dark mode

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -15,9 +15,15 @@ window.addEventListener("scroll", () =>{
 // theme colors
 const alternateStyles = document.querySelectorAll(".alternate-style");
 console.log(alternateStyles);
+
 function setActiveStyle(color){
+  localStorage.setItem("color", color);
+  changeColor();
+}
+
+function changeColor(){
   alternateStyles.forEach((style) =>{
-    if(color == style.getAttribute("title")){
+    if(localStorage.getItem("color") == style.getAttribute("title")){
       style.removeAttribute("disabled");
     }
     else{
@@ -26,19 +32,46 @@ function setActiveStyle(color){
   })
 }
 
-// theme ligth and dark mode
+// checking if 'color' key exists
+if(localStorage.getItem("color") !== null){
+  changeColor();
+}
+
+// theme light and dark mode
 const dayNight = document.querySelector(".day-night");
 
   dayNight.addEventListener("click", () =>{
-    dayNight.querySelector("i").classList.toggle("fa-sun");
-    dayNight.querySelector("i").classList.toggle("fa-moon");
     document.body.classList.toggle("dark");
+    if(document.body.classList.contains("dark")){
+      localStorage.setItem("theme", "dark");
+    }
+    else{
+      localStorage.setItem("theme", "light");
+    }
+    updateIcon();
   })
 
-window.addEventListener("load", () =>{
-  if(document.body.classList.contains("dark")){
-    dayNight.querySelector("i").classList.add("fa-sun");
-  }else{
-    dayNight.querySelector("i").classList.add("fa-moon");
+  function themeMode(){
+    // checking if 'theme' key exists
+    if(localStorage.getItem("theme") !== null){
+      if(localStorage.getItem("theme") === "light"){
+        document.body.classList.remove("dark");
+      }
+      else{
+        document.body.classList.add("dark");
+      }
+    }
+    updateIcon();
   }
-})
+  themeMode();
+
+  function updateIcon(){
+    if(document.body.classList.contains("dark")){
+      dayNight.querySelector("i").classList.remove("fa-moon");
+      dayNight.querySelector("i").classList.add("fa-sun");
+    }
+    else{
+      dayNight.querySelector("i").classList.remove("fa-sun");
+      dayNight.querySelector("i").classList.add("fa-moon");
+    }
+  }
